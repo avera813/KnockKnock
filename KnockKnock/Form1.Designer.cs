578013323e75887b11fc5f2842440653365bd522
v-alexve <v-alexve@AVERA-WIN10>
2016-09-22 16:48:53 -0700

    Add project files.

---
+++ KnockKnock/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace KnockKnock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.submitButton = new System.Windows.Forms.Button();
+            this.labelText = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // submitButton
+            // 
+            this.submitButton.AutoSize = true;
+            this.submitButton.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.submitButton.Location = new System.Drawing.Point(94, 118);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(94, 27);
+            this.submitButton.TabIndex = 0;
+            this.submitButton.Text = "Who\'s there?";
+            this.submitButton.UseVisualStyleBackColor = true;
+            this.submitButton.Click += new System.EventHandler(this.submitButton_Click);
+            // 
+            // labelText
+            // 
+            this.labelText.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelText.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelText.Location = new System.Drawing.Point(-2, 70);
+            this.labelText.Margin = new System.Windows.Forms.Padding(0);
+            this.labelText.Name = "labelText";
+            this.labelText.Size = new System.Drawing.Size(286, 21);
+            this.labelText.TabIndex = 1;
+            this.labelText.Text = "Knock, Knock!";
+            this.labelText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 211);
+            this.Controls.Add(this.labelText);
+            this.Controls.Add(this.submitButton);
+            this.Name = "Form1";
+            this.Text = "Knock Knock";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button submitButton;
+        private System.Windows.Forms.Label labelText;
+    }
+}
+
---
+++ KnockKnock/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KnockKnock
+{
+    public partial class Form1 : Form
+    {
+        class Joke
+        {
+            public string labelText;
+            public string buttonText;
+        }
+
+        int clickCount = 1;
+        List<Joke> jokes = new List<Joke>()
+        {
+            new Joke() { labelText = "Knock, Knock!", buttonText = "Who's there?" },
+            new Joke() { labelText = "Abby", buttonText = "Abby who?" },
+            new Joke() { labelText = "Abby New Year!", buttonText = "Start Over" },
+            new Joke() { labelText = "Knock, Knock!", buttonText = "Who's there?" },
+            new Joke() { labelText = "Anee", buttonText = "Anee who?" },
+            new Joke() { labelText = "Anee one you like!", buttonText = "Start Over" },
+            new Joke() { labelText = "Knock, Knock!", buttonText = "Who's there?" },
+            new Joke() { labelText = "Robin", buttonText = "Robin who?" },
+            new Joke() { labelText = "Robin the piggy bank again!", buttonText = "Start Over" }
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void submitButton_Click(object sender, EventArgs e)
+        {
+            labelText.Text = jokes.ElementAt(clickCount).labelText;
+            submitButton.Text = jokes.ElementAt(clickCount).buttonText;
+            clickCount++;
+            if (clickCount == jokes.Count())
+            {
+                clickCount = 0;
+            }
+        }
+    }
+}
